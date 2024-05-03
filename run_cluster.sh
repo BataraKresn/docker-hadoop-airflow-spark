fd4aed698c96e4200633ddb520eab4a31ed3ec10
BatraKresn <adimaspangestu02@gmail.com>
2024-05-03 11:28:00 +0700

    update

--- run_cluster.sh
+++ run_cluster.sh
@@ -6,11 +6,12 @@ if ! docker network inspect hadoop_network &>/dev/null; then
   docker network create hadoop_network
 fi
 
+# --no-cache
 # build docker image with image name hadoop-base:3.3.6
-docker build -t hadoop-base:3.3.6 -f Dockerfile-hadoop .
+docker build --no-cache -t hadoop-base:3.3.6 -f Dockerfile-hadoop .
 
 # running image to container, -d to run it in daemon mode
-docker compose -f docker-compose-hadoop.yml up -d
+docker compose --no-cache -f docker-compose-hadoop.yml up -d
 
 # cd postgres
 # docker build -t postgres:10.3 -f Dockerfile-postgres .
@@ -22,7 +23,7 @@ if [[ "$PWD" != "airflow" ]]; then
 fi
 
 # docker compose -f docker-compose-airflow.yml up -d
-docker compose -f docker-compose-airflow.yml up -d 
+docker compose --no-cache -f docker-compose-airflow.yml up -d 
 
 # Run Spark Cluster
 if [[ "$PWD" != "spark" ]]; then
